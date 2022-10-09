becaa1154da806b1416d8e27470ce41a4b346771
COLIN TAN KAH KIEE <104512800+BestStudent0202@users.noreply.github.com>
2022-10-09 10:17:03 +0800

    First Patched Using HTML_ESCAPE

---
+++ Second_Order_XSS_Patched/index.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html>
+<head>
+<link rel="icon" href="./images/favicon.png" type="image/png" sizes="16x16">
+<title>forum</title>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+	<script src="main.js"></script>
+</head>
+
+<div class="container">
+
+<div class="panel panel-default" style="margin-top:50px">
+  <div class="panel-body">
+    <h3>Comment Section</h3>
+    <hr>
+    <form name="frm" method="post">
+        <input type="hidden" id="commentid" name="Pcommentid" value="0">
+	<div class="form-group">
+	  <label for="usr">Write your name:</label>
+	  <input type="text" class="form-control" name="name" required>
+	</div>
+    <div class="form-group">
+      <label for="comment">Write your comments:</label>
+      <textarea class="form-control" rows="5" name="msg" required></textarea>
+    </div>
+	 <input type="button" id="butsave" name="save" class="btn btn-primary" value="Send">
+  </form>
+  </div>
+</div>
+  
+
+<div class="panel panel-default">
+  <div class="panel-body">
+    <h4>Comments</h4>           
+	<table class="table" id="MyTable" style="background-color: #edfafa; border:0px;border-radius:10px">
+	  <tbody id="record">
+		
+	  </tbody>
+	</table>
+  </div>
+</div>
+
+</div>
+
+</body>
+</html>
